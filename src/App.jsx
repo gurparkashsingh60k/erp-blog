c018fc151c2b8ac8b62486ab5c17d58d402f5c0a
deepakgupta60kk <deepakgupta60kk@gmail.com>
2024-03-10 13:15:38 +0530

    secondFlexChanges

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
 function App() {
 
   return (
     <>
-    <h1>Flag Changes</h1>     
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/signin' element={<SignIn/>} />
+          <Route path='/about' element={<About/>} />
+          <Route path='/signup' element={<SignUp/>} />
+        </Routes>
+      </BrowserRouter>     
     </>
   )
 }
